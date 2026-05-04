4e96bfa2fa274876b8f43ab7dcba33130a8e92d0
wilsonz0 <wilsonzhang1313@gmail.com>
2026-05-03 23:56:32 -0400

    add the start of the ui hotbar

--- Assets/Scripts/UI_Hotbar.cs
+++ Assets/Scripts/UI_Hotbar.cs
@@ -1,22 +1,22 @@
 using UnityEngine;
 
-public class UI_CraftingMenu : MonoBehaviour {
+public class UI_Hotbar : MonoBehaviour {
     private readonly float _outputSlotCellSize = 40f;
     private readonly float _inputSlotCellSize = 30f;
 
     private Inventory inventory;
 
-    public void RefreshCraftingMenu() {
-        Debug.Log("Refreshed crafting menu");
+    public void RefreshHotbar() {
+        Debug.Log("Refreshed hotbar");
     }
     
     public void SetInventory(Inventory inventory) {
         this.inventory = inventory;
         inventory.OnItemListChanged += OnItemListChangedEvent;
-        RefreshCraftingMenu();
+        RefreshHotbar();
     }
 
     private void OnItemListChangedEvent(object sender, System.EventArgs e) {
-        RefreshCraftingMenu();
+        RefreshHotbar();
     }
 }
